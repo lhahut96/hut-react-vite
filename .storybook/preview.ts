52f814b42569abde192296593183bfe1333f74ea
Tu Le <tu.le@designerjourneys.com>
2023-05-23 12:17:11 +0700

    prettier fix

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,9 @@
-import type { Preview } from "@storybook/react";
-import "../src/index.scss"
+import type { Preview } from '@storybook/react';
+import '../src/index.scss';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
